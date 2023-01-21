c0a20040925bca63fe5250734e729876083705ec
Maxi <maxi_toffolo@hotmail.com>
2023-01-20 21:26:28 -0300

    avances con js

--- js/index.js
+++ js/index.js
@@ -11,58 +11,118 @@ function solicitarUsuario() {
 
 solicitarUsuario()
 
-let cantidadRemeras = parseInt(prompt("Ingrese la cantidad de remeras que desea comprar"))
-console.log(cantidadRemeras)
-while (cantidadRemeras < 0) {
-    alert("No ingresaste ningun número para la cantidad de remeras")
-    cantidadRemeras = prompt("Por favor, ingrese la cantidad de remeras que desea comprar")
-    console.log(cantidadRemeras)
-}
-let precioRemera = 1800
-let totalRemeras = precioRemera * cantidadRemeras
-console.log("El total a pagar de las remeras es de" + " " + "$" + totalRemeras)
 
-let cantidadBermudas = parseInt(prompt("Ingrese la cantidad de bermudas que desea comprar"))
-console.log(cantidadBermudas)
-while (cantidadBermudas < 0) {
-    alert("No ingresaste ningun número para la cantidad de bermudas")
-    cantidadBermudas = prompt("Por favor, ingrese la cantidad de bermudas que desea comprar")
-    console.log(cantidadBermudas)
-}
-let precioBermuda = 2200
-let totalBermudas = precioBermuda * cantidadBermudas
-console.log("El Total a pagar de bermudas a pagar es" + " " + "$" + totalBermudas)
+class Prendas {
+    constructor(tipo, modelo, marca, color, precio) {
 
-let cantidadCamisas = parseInt(prompt("Ingrese la cantidad de camisas que desea comprar"))
-console.log(cantidadCamisas)
-while (cantidadCamisas < 0) {
-    alert("No ingresaste ningun número para la cantidad de camisas")
-    cantidadCamisas = prompt("Por favor, ingrese la cantidad de camisas que desea comprar")
-    console.log(cantidadCamisas)
-}
-let precioCamisa = 2800
-let totalCamisas = precioCamisa * cantidadCamisas
-console.log("El Total de camisas a pagar es" + " " + "$" + totalCamisas)
+        this.tipo = tipo,
+            this.modelo = modelo,
+            this.marca = marca,
+            this.color = color,
+            this.precio = precio
 
-let cantidadJeans = parseInt(prompt("Ingrese la cantidad de jeans que desea comprar"))
-console.log(cantidadJeans)
-while (cantidadJeans < 0) {
-    alert("No ingresaste ningun número para la cantidad de jeans")
-    cantidadJeans = prompt("Por favor, ingrese la cantidad de jeans que desea comprar")
-    console.log(cantidadJeans)
+    }
+    mostrarInfoPrenda() {
+        console.log(`Es un/a ${this.tipo}, modelo ${this.modelo} su marca es ${this.marca}, es de color ${this.color} y vale ${this.precio}`)
+    }
 }
-let precioJean = 3400;
-let totalJeans = precioJean * cantidadJeans;
-console.log("El Total de jeans a pagar es" + " " + "$" + totalJeans)
 
-let total = (totalRemeras + totalCamisas + totalJeans + totalBermudas);
-console.log("El precio total a pagar por todas las prendas es de" + " " + "$" + total)
+const prenda1 = new Prendas(`Remera`, `Oversize`, `Bando`, `Negro`, 4500)
+const prenda2 = new Prendas(`Remera`, `Musculosa`, `Guns`, `Gris`, 4000)
+const prenda3 = new Prendas(`Remera`, `Mangas Largas`, `Vandal`, `Blanco`, 4700)
+const prenda4 = new Prendas(`Bermuda`, `Algodon`, `Bando`, `Gris`, 5200)
+const prenda5 = new Prendas(`Bermuda`, `Mom`, `Vanda`, `Gris`, 5800)
+const prenda6 = new Prendas(`Bermuda`, `Mom`, `May`, `Negro`, 6000)
+const prenda7 = new Prendas(`Jeans`, `Gabardina`, `Hijos del Rey`, `Azul`, 7100)
+const prenda8 = new Prendas(`Jeans`, `Mom`, `Bando`, `Negro`, 7500)
+const prenda9 = new Prendas(`Jeans`, `Elastizado`, `Koda`, `Azul`, 7300)
+const prenda10 = new Prendas(`Camisa`, `Elastizada`, `Vandal`, `Bordo`, 6900)
+const prenda11 = new Prendas(`Camisa`, `Lino`, `Guns`, `Blanca`, 8000)
+const prenda12 = new Prendas(`Camisa`, `Cuello Mao`, `Hijos del Rey`, `Negro`, 6800)
 
-if (total >= 10000) {
-    console.log("Tu compra supera los $10000, por lo tanto tiene ENVIO GRATIS")
-    alert("Tu compra supera los $10000, por tanto tiene ENVIO GRATIS")
+const arrayPrendas = [prenda1, prenda2, prenda3, prenda4, prenda5, prenda6, prenda7, prenda8, prenda9, prenda10, prenda11, prenda12]
+
+
+function consultarCatalogo(arrayPrendas) {
+    (console.log`Nuestro catálogo es:`)
+    for (let elem of arrayPrendas) {
+        elem.mostrarInfoPrenda()
+    }
 }
-else if (total < 10000) {
-    console.log("Además de tu compra, el costo del envío es $350")
-    alert("Además de tu compra, el costo del envío es $350")
+
+
+function buscarCatalogoXprenda(arrayPrendas) {
+    let prendaBuscada = prompt("Ingrese el tipo de prenda que esta buscando:")
+    let buscar = arrayPrendas.filter(
+        (obj) => obj.tipo.toLowerCase() == prendaBuscada.toLowerCase()
+
+    )
+    console.log(buscar)
+    if (buscar.length == 0) {
+        console.log(`Para la prenda ${prendaBuscada} no hay coincidencias en nuestro catalogo`)
+        buscarCatalogoXprenda()
+    }
 }
+
+
+function buscarXcolor(arrayPrendas) {
+    let colorBuscado = prompt("Ingrese el color de prenda que esta buscando:")
+    let busqueda = arrayPrendas.filter(
+        (prenda) => prenda.color.toLowerCase() == colorBuscado.toLowerCase()
+
+    )
+    console.log(busqueda)
+    if (busqueda.length == 0) {
+        console.log(`Para el color ${colorBuscado} no hay coincidencias en nuestro catalogo`)
+        buscarCatalogoXprenda()
+    }
+}
+
+function buscarXprecio(arrayPrendas) {
+    let precioBuscado = parseInt(prompt("ingrese el precio máximo que pagaría por alguna prenda:"))
+    let investigarPrecio = arrayPrendas.filter(
+        (prenda) => prenda.precio <= precioBuscado)
+    console.log(investigarPrecio)
+}
+
+
+function menu() {
+    let salirMenu = false
+    do {
+        salirMenu = preguntarOpcion(salirMenu)
+    } while (!salirMenu)
+}
+
+function preguntarOpcion() {
+    let opcionIngresada = parseInt(prompt(`Ingrese la opción deseada:
+           1 - Consultar catálogo
+           2 - Buscar catálogo por tipo de prenda:
+           3 - Buscar prendas del mismo color:
+           4 - investigar catálogo por precio:
+           0 - Salir del menu`))
+
+    switch (opcionIngresada) {
+        case 1:
+            consultarCatalogo(arrayPrendas)
+            break
+        case 2:
+            buscarCatalogoXprenda(arrayPrendas)
+            break
+        case 3:
+            buscarXcolor(arrayPrendas)
+            break
+        case 4:
+            buscarXprecio(arrayPrendas)
+            break
+        case 0:
+            console.log("¡GRACIAS POR LA VISITA!")
+            alert("¡GRACIAS POR LA VISITA!")
+            salir = true
+            return salir
+        default:
+            console.log("Para continuar ingrese un número por favor")
+            break
+    }
+}
+
+preguntarOpcion()
